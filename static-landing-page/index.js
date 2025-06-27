3d03c5f6b3211f0daa7fd5cbe8c22bc87f395e29
Apekshakv3 <kvapeksha3@gmail.com>
2025-06-28 02:32:11 +0530

    Update index.js

--- static-landing-page/index.js
+++ static-landing-page/index.js
@@ -1,1 +1,12 @@
-
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Apidata from "./Apidata";
+const root = document.getElementById('root')
+const rootelement = ReactDOM.createRoot(
+  root
+)
+rootelement.render(
+    <App/>
+  
+)
